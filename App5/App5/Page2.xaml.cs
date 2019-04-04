95926d53aee3d2236b782745bd79a4331b874c6c
raja <g00351263@gmit.ie>
2019-04-04 19:09:27 +0100

    Camera working, Weather App completed with icon image, news page completed

--- App5/App5/Page2.xaml.cs
+++ App5/App5/Page2.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,42 +13,51 @@ namespace App5
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Page2 : ContentPage
 	{
-        RestService _restService;
+        RestService _restService; // instance of class for weather //
         public Page2()
         {
             InitializeComponent();
-            _restService = new RestService();
+            _restService = new RestService(); // intialising the weather data and method //
+
+
         }
 
-        async void OnGetWeatherButtonClicked(object sender, EventArgs e)
+        async void OnGetWeatherButtonClicked(object sender, EventArgs e) // getting weather data stored in weather object //
         {
             if (!string.IsNullOrWhiteSpace(_cityEntry.Text))
             {
                 WeatherData weatherData = await _restService.GetWeatherData(GenerateRequestUri(Constants.OpenWeatherMapEndpoint));
-                BindingContext = weatherData;
+                BindingContext = weatherData; // binding to use in xaml //
+                string w = weatherData.Weather[0].Icon; // getting the icon image extension from json //
+
+                string start = "http://openweathermap.org/img/w/"; // this has to be concatenated to icon name recieved by json //
+                string end= ".png"; // this is the end extension of image icon //
+
+                string complete = start + w + end; // joining the complete image to display in app //
+
+                ionic.Source = new UriImageSource // creating new image for xaml display
+                {
+                    Uri = new Uri(complete), // binding the json to image //
+                   
+                };
+
+                ionic.HeightRequest = 150; // on click little make icon bigger //
+                ionic.WidthRequest = 150; // same as above //
             }
         }
 
-        string GenerateRequestUri(string endpoint)
+        string GenerateRequestUri(string endpoint) // some options to join with the api string 
         {
-            string requestUri = endpoint;
-            requestUri += $"?q={_cityEntry.Text}";
-            requestUri += "&units=metric"; // or units=metric
-            requestUri += $"&APPID={Constants.OpenWeatherMapAPIKey}";
+            string requestUri = endpoint; // join api url with api key
+            requestUri += $"?q={_cityEntry.Text}"; // putting city in between url to get city //
+            requestUri += "&units=metric"; // or units=metric or imperial //
+            requestUri += $"&APPID={Constants.OpenWeatherMapAPIKey}"; // puttting api key to url //
             return requestUri;
         }
 
         private void BackToMainPage_Clicked(object sender, EventArgs e)
         {
-            Navigation.PushAsync(new MainPage());
-        }
-
-        private string completeImage1;
-
-        public string SetcompleteImage(string value)
-        {
-            completeImage1 = "http://openweathermap.org/img/w/" + value + ".png";
-            return completeImage1;
+            Navigation.PushAsync(new MainPage()); // bringin to main page once clicked
         }
     }
 }
